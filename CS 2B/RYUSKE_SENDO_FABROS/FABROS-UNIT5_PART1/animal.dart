14746f6c1781a092307e407f160eb84a978b56a7
Fabshkiee <pwedefabros@gmail.com>
2026-03-30 20:33:57 +0800

    feat: add walk method

--- CS 2B/RYUSKE_SENDO_FABROS/FABROS-UNIT5_PART1/animal.dart
+++ CS 2B/RYUSKE_SENDO_FABROS/FABROS-UNIT5_PART1/animal.dart
@@ -3,6 +3,15 @@ class Animal {
   String _kingdom;
   String _dob;
   int _numLegs;
-  
+
   Animal(this._name, this._kingdom, this._dob, this._numLegs);
+
+  // Walk Method
+  void walk(String direction) {
+    if (_numLegs > 0) {
+      print('$_name walks to the $direction');
+    } else {
+      print('$_name can\'t walk');
+    }
+  }
 }
