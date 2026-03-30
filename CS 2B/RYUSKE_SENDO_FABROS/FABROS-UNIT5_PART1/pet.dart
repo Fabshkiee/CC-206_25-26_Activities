b4178a25c8af3fea25d69551f0bf92fbbf5d8f97
Fabshkiee <pwedefabros@gmail.com>
2026-03-30 20:43:36 +0800

    feat: add pet methods

--- CS 2B/RYUSKE_SENDO_FABROS/FABROS-UNIT5_PART1/pet.dart
+++ CS 2B/RYUSKE_SENDO_FABROS/FABROS-UNIT5_PART1/pet.dart
@@ -5,5 +5,27 @@ class Pet extends Animal {
   int _kindness = 0;
 
   Pet(String name, String kingdom, String dob, int numLegs, this._nickname)
-      : super(name, kingdom, dob, numLegs);
+    : super(name, kingdom, dob, numLegs);
+
+  // kick pet method
+  void kick(int val) {
+    _kindness -= val;
+    print('$_nickname was kicked! kindness decreased by $val.');
+  }
+
+  // petting pet method
+  void pet(int val) {
+    if (_kindness < 0) {
+      print('FAILED: $_nickname is currently unfriendly.');
+    } else {
+      _kindness += val;
+      print('$_nickname was petted! kindness increased by $val.');
+    }
+  }
+
+  //feed pet method
+  void feed(int val) {
+    _kindness += val;
+    print('$_nickname was fed! kindness increased by $val.');
+  }
 }
