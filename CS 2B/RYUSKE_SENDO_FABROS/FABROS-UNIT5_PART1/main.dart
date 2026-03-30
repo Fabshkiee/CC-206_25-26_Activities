567b5012c7fa3ab9bad8c97ebe0d570373377a0e
Fabshkiee <pwedefabros@gmail.com>
2026-03-30 20:56:06 +0800

    feat: add main with animal list

---
+++ CS 2B/RYUSKE_SENDO_FABROS/FABROS-UNIT5_PART1/main.dart
@@ -0,0 +1,12 @@
+import 'Animal.dart';
+import 'pet.dart';
+
+void main() {
+  List<Animal> ZOO = [
+    Animal('Capybara', 'Animalia', '2015-05-10', 4),
+    Animal('T-Rex', 'Animalia', '2018-03-22', 2),
+    Animal('Mammoth', 'Animalia', '2020-07-15', 4),
+    Animal('Dodo', 'Animalia', '2010-11-25', 2),
+    Animal('Blobfish', 'Animalia', '2019-01-12', 0),
+  ];
+}
